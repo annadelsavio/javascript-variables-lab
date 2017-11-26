ff2be4242329dbcf6541d9c25f44fa58af4bd069
Alexandra Ackerman <aaackerman.dev@gmail.com>
2017-11-26 13:47:07 -0500

    Make instructions make sensE

--- js/script.js
+++ js/script.js
@@ -7,22 +7,26 @@ $(function() {
   // 4) Create a variable with your year of graduation
   // 5) Create a variable with the current year
   // 6) Create a variable called yearsUntilGraduation with the difference between the two years
-  // 7) Create a variable called greaterThanMedian that returns a boolean value for whether the new school median
-  //     is greater than the NYC median income
-  // 8) Create a variable called medianDifference that subtracts new school median income from nyc
-  // 9) Check out your mad libs in the browser!
-  // 10) Change some of the variables, reload your index page, and watch the information change
 
 
   var newSchoolMedian = 39400;
   var nycMedian = 49461;
 
 
+  // 7) Create a variable called greaterThanMedian that returns a boolean value for whether the new school median
+  //     is greater than the NYC median income
+  // 8) Create a variable called medianDifference that subtracts new school median income from nyc
+
+
   // All of your code needs to go above this!
   $("#full-name").text(fullName);
-  $("#years-until-graduation").text(yearsUntilGraduation);
+
+  // 9) Replace #uears-until-graduation text with yearsUntilGraduation number
+
   $("#median").text(newSchoolMedian);
   $("#ny-median").text(nycMedian);
   $("#median-difference").text(medianDifference);
   $("#median-boolean").text(greaterThanMedian);
+
+  // 10) Change some of the variables, reload your index page, and watch the information change
 });
